f23da84dc92a47345136b355608da8d9f44071a4
AlexandrStopkin <stnalex1@gmail.com>
2023-04-18 10:24:15 +0200

    Add sortByDate(), sortByUser(), sortByCategory()

--- src/classes/Operations.java
+++ src/classes/Operations.java
@@ -1,6 +1,5 @@
 package classes;
 
-
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
@@ -11,12 +10,12 @@ public class Operations {
   /**
    * Calculate payment balance in period
    *
-   * @param records List of Record with payments
+   * @param records   List of Record with payments
    * @param dateBegin Begin of payments period
-   * @param dateEnd End of payments period
+   * @param dateEnd   End of payments period
    * @return Sum of payments in this period
    */
-  public static double calcBalance (List<Record> records, Date dateBegin, Date dateEnd){
+  public static double calcBalance(List<Record> records, Date dateBegin, Date dateEnd) {
     return records.stream()
         .filter(x -> x.getDate().after(dateBegin))
         .filter(x -> x.getDate().before(dateEnd))
@@ -25,14 +24,14 @@ public class Operations {
   }
 
   /**
-   * Sort part of LIst of Records in period
+   * Sort part of LIst of Records by ID in period
    *
-   * @param records List of Record with payments
+   * @param records   List of Record with payments
    * @param dateBegin Begin of payments period
-   * @param dateEnd End of payments period
+   * @param dateEnd   End of payments period
    * @return new sorted LIst of Records in period
    */
-  public static List<Record> sortByID (List<Record> records, Date dateBegin, Date dateEnd){
+  public static List<Record> sortByID(List<Record> records, Date dateBegin, Date dateEnd) {
     List<Record> result;
     result = records.stream()
         .filter(x -> x.getDate().after(dateBegin))
@@ -43,16 +42,72 @@ public class Operations {
   }
 
   /**
-   * Sort part of LIst of ALL Records
+   * Sort part of LIst of by ID ALL Records
    *
    * @param records List of Record with payments
    * @return new sorted LIst of Records
    */
-  public static List<Record> sortByID (List<Record> records){
+  public static List<Record> sortByID(List<Record> records) {
     List<Record> result;
     result = records.stream()
         .sorted(Comparator.comparingInt(Record::getId))
         .collect(Collectors.toList());
     return result;
   }
+
+  /**
+   * Sort part of LIst of Records by Date in period
+   *
+   * @param records   List of Record with payments
+   * @param dateBegin Begin of payments period
+   * @param dateEnd   End of payments period
+   * @return new sorted LIst of Records
+   */
+  public static List<Record> sortByDate(List<Record> records, Date dateBegin, Date dateEnd) {
+    List<Record> result;
+    result = records.stream()
+        .filter(x -> x.getDate().after(dateBegin))
+        .filter(x -> x.getDate().before(dateEnd))
+        .sorted(Comparator.comparing(Record::getDate))
+        .collect(Collectors.toList());
+    return result;
+  }
+
+  /**
+   * Sort part of LIst of Records by User in period
+   *
+   * @param records   List of Record with payments
+   * @param dateBegin Begin of payments period
+   * @param dateEnd   End of payments period
+   * @return new sorted LIst of Records
+   */
+  public static List<Record> sortByUser(List<Record> records, Date dateBegin, Date dateEnd) {
+    List<Record> result;
+    result = records.stream()
+        .filter(x -> x.getDate().after(dateBegin))
+        .filter(x -> x.getDate().before(dateEnd))
+        .sorted(Comparator.comparing(Record::getUser))
+        .collect(Collectors.toList());
+    return result;
+  }
+
+  /**
+   * Sort part of LIst of Records by Category in period
+   *
+   * @param records   List of Record with payments
+   * @param dateBegin Begin of payments period
+   * @param dateEnd   End of payments period
+   * @return new sorted LIst of Records
+   */
+  public static List<Record> sortByCategory(List<Record> records, Date dateBegin, Date dateEnd) {
+    List<Record> result;
+    result = records.stream()
+        .filter(x -> x.getDate().after(dateBegin))
+        .filter(x -> x.getDate().before(dateEnd))
+        .sorted(Comparator.comparing(Record::getCategory))
+        .collect(Collectors.toList());
+    return result;
+  }
+
+
 }

e0f386f31dd9e1b8ec4ad97195d734385aa44b35
AlexandrStopkin <stnalex1@gmail.com>
2023-04-18 11:41:18 +0200

    Add List of Categories

--- src/BudgetTracker.java
+++ src/BudgetTracker.java
@@ -1,8 +1,10 @@
+import classes.Category;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BudgetTracker {
     public static List<Record> records= new ArrayList<>();
+    public static List<Category> categories = new ArrayList<>();
     public static void main(String[] args) {
 
     }

7255f08be05a434e9fc8833c8ad3a57f1fb93e6d
AlexandrStopkin <stnalex1@gmail.com>
2023-04-18 09:57:34 +0200

    Add sortByID() two tipes with and without Date period

--- src/classes/Operations.java
+++ src/classes/Operations.java
@@ -1,8 +1,10 @@
 package classes;
 
 
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Operations {
 
@@ -14,12 +16,43 @@ public class Operations {
    * @param dateEnd End of payments period
    * @return Sum of payments in this period
    */
-
   public static double calcBalance (List<Record> records, Date dateBegin, Date dateEnd){
     return records.stream()
         .filter(x -> x.getDate().after(dateBegin))
         .filter(x -> x.getDate().before(dateEnd))
         .mapToDouble(Record::getAmount)
         .sum();
+  }
+
+  /**
+   * Sort part of LIst of Records in period
+   *
+   * @param records List of Record with payments
+   * @param dateBegin Begin of payments period
+   * @param dateEnd End of payments period
+   * @return new sorted LIst of Records in period
+   */
+  public static List<Record> sortByID (List<Record> records, Date dateBegin, Date dateEnd){
+    List<Record> result;
+    result = records.stream()
+        .filter(x -> x.getDate().after(dateBegin))
+        .filter(x -> x.getDate().before(dateEnd))
+        .sorted(Comparator.comparingInt(Record::getId))
+        .collect(Collectors.toList());
+    return result;
+  }
+
+  /**
+   * Sort part of LIst of ALL Records
+   *
+   * @param records List of Record with payments
+   * @return new sorted LIst of Records
+   */
+  public static List<Record> sortByID (List<Record> records){
+    List<Record> result;
+    result = records.stream()
+        .sorted(Comparator.comparingInt(Record::getId))
+        .collect(Collectors.toList());
+    return result;
   }
 }

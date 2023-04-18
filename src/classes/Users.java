f47a926666b37fd67513c6aa857a17041733e027
AlexandrStopkin <stnalex1@gmail.com>
2023-04-18 17:51:50 +0200

    Add List<Users>

--- src/classes/Users.java
+++ src/classes/Users.java
@@ -2,6 +2,10 @@ package classes;
 
 public class Users {
   private String name;
+  private boolean child;
+
+  public Users() {};
+
   // TODO make real class with users and make List<Users>
   public String getName() {
     return this.name;

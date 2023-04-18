27071cddeea7d373a4b16f793d599317b557b717
AlexandrStopkin <stnalex1@gmail.com>
2023-04-18 14:27:21 +0200

    Add dateToString()

--- src/classes/Operations.java
+++ src/classes/Operations.java
@@ -1,11 +1,31 @@
 package classes;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Operations {
+
+  /**
+   * Convert Date to String with pattern "dd.MM.yyyy"
+   *
+   * @param date date to be converted
+   * @return String of date according to pattern
+   */
+  public static String dateToString (Date date){
+    DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    String strDate = dateFormat.format(date);
+    return strDate;
+  }
+
+  public static Date StringToDate (String string){
+
+    return ;
+  }
 
   /**
    * Calculate payment balance in period

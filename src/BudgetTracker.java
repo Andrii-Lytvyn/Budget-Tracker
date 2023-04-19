765c88d75ae2cb8a0dac88697e2182a8c7608d9d
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-04-19 19:40:55 +0200

    Add write read cryptoFile

--- src/BudgetTracker.java
+++ src/BudgetTracker.java
@@ -14,15 +14,16 @@ public class BudgetTracker {
   public static List<Users> users = new ArrayList<>();
 
   public static void main(String[] args) throws IOException, ParseException {
-    File budgetFile = new File("src/res/budget.txt");
+    // File budgetFile = new File("src/res/budget.txt");
     File loginFile = new File("src/res/login.txt");
-    I_O_Crypto.parseFileUncrypted(budgetFile, records);
+    File crypto = new File("src/res/crypto.txt");
+    // I_O_Crypto.parseFileUncrypted(budgetFile, records);
     Record record = new Record();
-    //I_O_Crypto.makeCrypto(records);
+    I_O_Crypto.makeCrypto(records);
 
     //Users.makeCrypto(Users.getLoginFromFile(loginFile));
 
-     Users.showLogin(loginFile);
+    Users.showLogin(loginFile);
     System.out.println(Users.getUserName());
     Category c0 = new Category(0, "Food", false);
     categories.add(c0);
@@ -46,14 +47,31 @@ public class BudgetTracker {
     categories.add(c9);
 //    Submenu.addRecord(records, categories);
 
-   // Users.getLoginFromFile(loginFile);
+    // Users.getLoginFromFile(loginFile);
 
 //    Submenu.addRecord(records,categories);
 //    Submenu.addRecord(records, categories);
 //    Submenu.editRecord(records, categories);
 //    Submenu.deleteRecord(records);
+    //Users.getLoginFromFile(loginFile);
+    //  System.out.println("Users.userNames = " + Users.userNames);
 //    Output.chartCategory(records, categories, Operations.stringToDate("01.01.2000"), Operations.stringToDate("20.04.2023"));
 //    Output.chartUser(records, users, Operations.stringToDate("01.01.2000"), Operations.stringToDate("20.04.2023"));
+
+
+//    //Submenu.addRecord(records,categories);
+//    I_O_Crypto.makeCrypto(records);//Шифрую записи
+//    I_O_Crypto.makeOutputCryptoFile(I_O_Crypto.list);//делаю файл из зашифрованного листа и пишу в файл крипто
+//    I_O_Crypto.addInfoFromFileToList(crypto);
+//    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++");
+//
+//    I_O_Crypto.makeUnCrypt(crypto,records);
+//    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+    // records.forEach(System.out::println);
+    I_O_Crypto.makeRecordsFromCryptoFile(crypto, records);
+    I_O_Crypto.makeNewOutputCryptoFile(records, I_O_Crypto.list);
+
     Output.chartDate(records, Operations.stringToDate("01.04.2023"), Operations.stringToDate("20.04.2023"));
   }
 }
--- src/classes/I_O_Crypto.java
+++ src/classes/I_O_Crypto.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public class I_O_Crypto {
 
-  final static int CRYPTO = 1;
+  final static int CRYPTO = 12;
   final public static String SEP = ",";
-  final static List<String> list = new ArrayList<>();
+  public static List<String> list = new ArrayList<>();
 
   /**
    * Overwrites the sheet character-by-character by shifting the ASCII code by the number when CRYPTO > 0 to the right
@@ -19,10 +19,11 @@ public class I_O_Crypto {
    * @param records hand over the sheet for encryption
    */
   public static void makeCrypto(List<Record> records) {
+    list.clear();
     StringBuilder tempString = new StringBuilder();
     int j = 0;
     for (Record items : records) {
-      String line = recordToString(items);
+      String line = recordToCryptoString(items);
       for (int i = 0; i < line.length(); i++) {
         int code = (int) line.charAt(i) + CRYPTO;
         tempString.append((char) code);
@@ -31,7 +32,7 @@ public class I_O_Crypto {
       tempString = new StringBuilder();
       j++;
     }
-    list.forEach(System.out::println);
+   // list.forEach(System.out::println);
   }
 
   /**
@@ -46,7 +47,7 @@ public class I_O_Crypto {
     for (String row = inputFileReader.readLine(); row != null; row = inputFileReader.readLine()) {
       StringBuilder tempString = new StringBuilder();
       for (int i = 0; i < row.length(); i++) {
-        int code = (int) row.charAt(i)/* - CRYPTO*/;
+        int code = (int) row.charAt(i) - CRYPTO;
         tempString.append((char) code);
       }
       list.add(tempString.toString());
@@ -97,7 +98,7 @@ public class I_O_Crypto {
       record.setCategory(temp[4]);
       record.setComment(temp[5]);
       records.add(record);
-      System.out.println(recordToString(record));
+     // System.out.println(recordToString(record));
       return;
     }
   }
@@ -133,7 +134,10 @@ public class I_O_Crypto {
     return record.getId() + "," + Operations.dateToString(record.getDate()) + "," + record.getUser() + "," + record.getAmount() +
             "," + record.getCategory() + "," + record.getComment() + "\n";
   }
-
+  public static String recordToCryptoString(Record record) {
+    return record.getId() + "," + Operations.dateToString(record.getDate()) + "," + record.getUser() + "," + record.getAmount() +
+            "," + record.getCategory() + "," + record.getComment();
+  }
   /**
    * Make output file from record list.
    *
@@ -143,9 +147,11 @@ public class I_O_Crypto {
 
   public static void makeOutputFile(List<Record> records) throws IOException {
     try {
+      makeCrypto(records);
       FileWriter fileWriter = new FileWriter("src/res/budget.txt");
       for (Record r : records) {
-        fileWriter.write(recordToString(r));
+
+        fileWriter.write(recordToCryptoString(r));
       }
       fileWriter.close();
     } catch (FileNotFoundException e) {
@@ -156,6 +162,18 @@ public class I_O_Crypto {
     }
   }
 
+public static void makeNewOutputCryptoFile(List<Record> records,List<String> list) throws IOException {
+
+  makeCrypto(records);
+  makeOutputCryptoFile(list);
+
+}
+  public static void makeRecordsFromCryptoFile(File cryptoFile, List <Record> list) throws IOException, ParseException {
+    makeUnCrypt(cryptoFile,list);
+  }
+
+
+
   public static void parseFileUncrypted(File cryptoFile, List<Record> records) throws IOException, ParseException {
     //list.clear();
     BufferedReader inputFileReader = new BufferedReader(new FileReader(cryptoFile));
@@ -163,6 +181,25 @@ public class I_O_Crypto {
       parseRecordFromString(row, records);
     }
   }
-
+  public static void makeOutputCryptoFile(List<String> list) throws IOException {
+    try {
+      FileWriter cryptoFile = new FileWriter("src/res/crypto.txt");
+      for (String items : list) {
+        cryptoFile.write(items + "\n");
+      }
+      cryptoFile.close();
+    } catch (FileNotFoundException e) {
+      FileWriter cryptoFile = new FileWriter("src/res/crypto.txt");
+    } catch (IOException e) {
+      System.err.println("Input/output exception: " + e.getMessage());
+    }
+  }
 
 }
+/*1,10.04.2023,Mother,-200.0,Food,coca-cola
+2,12.04.2023,Father,-10.0,Party,hot-dog
+3,13.04.2023,papa,-127.0,House,carpet
+5,14.04.2023,papa,-50.0,Clothes,brod
+7,15.04.2023,mama,-15.51,Food,fish
+8,19.04.2023,mama,1500.0,,salary for March
+*/
